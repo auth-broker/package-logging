f1ef5d2b73c5e188f609409e9b844fb08f228269
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2025-10-05 13:37:17 +1100

    feat: support logging enhancements

--- tests/ab_core/logging/test_config.py
+++ tests/ab_core/logging/test_config.py
@@ -12,7 +12,7 @@ def test_logging_config():
 
     logger = logging.getLogger("ab_service.blah")
     logger.debug("Test DEBUG log from test case! (this shouldn't show)")
-    logger.info("Test INFO log from test case! (this should show)")
+    logger.info("Test INFO log from test case! (this should show)", extra={"key": "value"})
     logger.warning("Test WARNING log from test case! (this should show)")
     logger.error("Test ERROR log from test case! (this should show)")
     try:
